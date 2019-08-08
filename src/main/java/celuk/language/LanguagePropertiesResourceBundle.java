9a596c47e5e6eb624986db39c178067792078563
Tony Aldhous <tonyaldhous@cel-uk.com>
2019-08-08 16:05:59 +0100

    Initial commit.

---
+++ src/main/java/celuk/language/LanguagePropertiesResourceBundle.java
@@ -0,0 +1,195 @@
+package celuk.language;
+
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Locale;
+import java.util.Map;
+import java.util.MissingResourceException;
+import java.util.ResourceBundle;
+import java.util.Set;
+
+public abstract class LanguagePropertiesResourceBundle extends ResourceBundle
+{
+    /**
+     * The base name for the ResourceBundles to load in.
+     */
+    private String baseName;
+
+    /**
+     * The package name where the properties files should be.
+     */
+    private Path baseDirectory;
+
+    /**
+     * The package name where the properties files should be.
+     */
+    private String languageFolderName = null;
+
+    
+    /**
+     * A Map containing the combined resources of all parts building this
+     * MultiplePropertiesResourceBundle.
+     */
+    private Map<String, Object> combined;
+    
+    private final Set<Locale> availableLocales = new HashSet<>();
+
+    /**
+     * Construct a <code>MultiplePropertiesResourceBundle</code> for the passed
+     * in base-name.
+     *
+     * @param baseName the base-name that must be part of the properties file
+     * names.
+     */
+    protected LanguagePropertiesResourceBundle(String baseName)
+    {
+        this(null, baseName);
+    }
+
+    /**
+     * Construct a <code>MultiplePropertiesResourceBundle</code> for the passed
+     * in base-name.
+     *
+     * @param packageName the package name where the properties files should be.
+     * @param baseName the base-name that must be part of the properties file
+     * names.
+     */
+    protected LanguagePropertiesResourceBundle(String packageName, String baseName)
+    {
+        this(packageName, "", baseName);
+    }
+
+    /**
+     * Construct a <code>MultiplePropertiesResourceBundle</code> for the passed
+     * in base-name.
+     *
+     * @param baseDirectory the package name where the properties files should
+     * be.
+     * @param languageFolderName
+     * @param baseName the base-name that must be part of the properties file
+     * names.
+     */
+    protected LanguagePropertiesResourceBundle(String baseDirectory,
+            String languageFolderName,
+            String baseName)
+    {
+        this.baseDirectory = Paths.get(baseDirectory).normalize();
+        
+        this.languageFolderName = languageFolderName;
+        this.baseName = baseName;
+        
+        loadBundlesOnce();
+    }
+    
+    @Override
+    public Object handleGetObject(String key)
+    {
+        if (key == null)
+        {
+            throw new NullPointerException();
+        }
+        loadBundlesOnce();
+        return combined.get(key);
+    }
+    
+    @Override
+    public Enumeration<String> getKeys()
+    {
+        loadBundlesOnce();
+        ResourceBundle parent = this.parent;
+        return new ResourceBundleEnumeration(combined.keySet(), (parent != null) ? parent.getKeys()
+                : null);
+    }
+    
+    private void addBundleData(Path resourcePath, String resourceName)
+    {
+        ResourceBundle bundle = null;
+        try
+        {
+            File propFile = resourcePath.toFile();
+            
+            if (propFile.exists())
+            {
+                URL[] urlsToSearch =
+                {
+                    propFile.toURI().toURL()
+                };
+                URLClassLoader cl = new URLClassLoader(urlsToSearch);
+                
+                bundle = ResourceBundle.getBundle(resourceName, I18n.getApplicationLocale(), cl, new UTF8Control());
+                Enumeration<String> keys = bundle.getKeys();
+                String key = null;
+                while (keys.hasMoreElements())
+                {
+                    key = keys.nextElement();
+                    combined.put(key, bundle.getObject(key));
+                }
+            }
+        } catch (MalformedURLException ex)
+        {
+            System.err.println("Failed to load multi-language data");
+        }
+    }
+
+    /**
+     * Load the resources once.
+     */
+    private void loadBundlesOnce()
+    {
+        if (combined == null)
+        {
+            combined = new HashMap<String, Object>(128);
+            
+            Path specifiedResourcePath = baseDirectory.resolve(languageFolderName);
+            
+            addBundleData(specifiedResourcePath, baseName);
+        }
+    }
+    
+    private void addAvailableLocales(String resourcePath)
+    {
+        
+        File commonDir = new File(resourcePath);
+        
+        availableLocales.add(Locale.ENGLISH);
+        
+        String[] filenamesToIngest = commonDir.list();
+        
+        if (filenamesToIngest != null)
+        {
+            for (String filename : filenamesToIngest)
+            {
+                filename = filename.replaceFirst(".properties", "");
+                
+                Locale locale = null;
+                
+                String[] languageStringParts = filename.split("_");
+                
+                switch (languageStringParts.length)
+                {
+                    case 2:
+                        locale = new Locale(languageStringParts[1]);
+                        availableLocales.add(locale);
+                        break;
+                    case 3:
+                        locale = new Locale(languageStringParts[1], languageStringParts[2]);
+                        availableLocales.add(locale);
+                        break;
+                }
+            }
+        }
+    }
+    
+    public Set<Locale> getAvailableLocales()
+    {
+        return availableLocales;
+    }
+}
